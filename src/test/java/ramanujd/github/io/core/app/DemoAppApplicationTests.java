b683565980fc7247e9f773438cbbf4ba82b6d3f6
ramanujd <rampooja245@gmail.com>
2022-09-16 11:39:45 +0530

    Initial commit

---
+++ src/test/java/ramanujd/github/io/core/app/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ramanujd.github.io.core.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
