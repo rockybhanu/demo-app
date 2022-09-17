2a64098e4f03d7578df5bc66bbded269eef6a8b0
ramanujd <rampooja245@gmail.com>
2022-09-17 16:14:18 +0530

    Api Written for Instrument Operation

--- src/main/java/ramanujd/github/io/core/app/repo/PhoneDetailsRepository.java
+++
@@ -1,7 +0,0 @@
-package ramanujd.github.io.core.app.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ramanujd.github.io.core.app.model.PhoneDetails;
-
-public interface PhoneDetailsRepository extends JpaRepository<PhoneDetails, Long> {
-}
